6ef964d1407911be3dd996b1280090bf681ec106
Dplant <dean.plant@greenpeace.org>
2016-01-21 10:03:50 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,10 +2,12 @@
 myvar='He said "hello".'
 myage=50
 mylist=['this','that','theother']
-mynumlist=[1.15.25.35]
+mynumlist=[1,15,25,35]
 print myvar
 print myage
 listlength=len(mylist)
 stringlength=len(myvar)
 numlength=len(myage)
-print len
+print len(mylist)
+print len(myvar)
+print len(myage)
