62bd0f455612afaf7fa8d8083dd9d3b2c2cce243
Dplant <dean.plant@greenpeace.org>
2016-01-21 09:59:15 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,29 +1,11 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+
 myvar='He said "hello".'
 myage=50
 mylist=['this','that','theother']
-mynumlist=[1,15,25,35]
+mynumlist=[1.15.25.35]
 print myvar
 print myage
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+listlength=len(mylist)
+stringlength=len(myvar)
+numlength=len(myage)
+print len
